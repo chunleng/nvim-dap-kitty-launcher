c787732dcfa19e739671d14e84bce9df4400eed6
Lim Chun Leng <limchunleng@gmail.com>
2023-05-27 22:48:19 +0900

    refactor: apply autoformatter

--- lua/kitty-launcher/init.lua
+++ lua/kitty-launcher/init.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 function M.setup()
-    local script_path = vim.fn.resolve(debug.getinfo(1).source .. '/../../../bin/kitty_launcher.sh'):sub(2)
-    require('dap').defaults.fallback.external_terminal = {
-        command = script_path
-    }
+    local script_path = vim.fn.resolve(debug.getinfo(1).source ..
+                                           '/../../../bin/kitty_launcher.sh'):sub(
+                            2)
+    require('dap').defaults.fallback.external_terminal = {command = script_path}
 end
 
 return M
